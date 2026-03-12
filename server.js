13af733c55d84e2adce1e982b2d174eede905cfd
azo1417-arch <azo.1417@hotmail.com>
2026-03-12 20:12:47 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cron = require('node-cron');
 const axios = require('axios');
 
 const app = express();
+app.use((req,res,next)=>{res.header('Access-Control-Allow-Origin','*');res.header('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE,OPTIONS');res.header('Access-Control-Allow-Headers','Content-Type');if(req.method==='OPTIONS')return res.sendStatus(200);next();});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
